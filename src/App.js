7e8cf5b72a606f5d51e4c4d38c83e5c24b55b2be
Bradley Kingsley <bradleykigsley2@gmail.com>
2020-02-15 10:35:29 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {filterByValue, loadData, loadExactPage, loadNewPage, sortByAlphabet, sortByPrice} from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    componentDidMount() {
+        const params = new URLSearchParams(window.location.search);
+        const pageQueryParam = params.get('page');
+        if (!pageQueryParam) {
+            window.history.pushState({page: 1}, "title 1", "?page=1");
+        } else {
+
+        }
+        this.props.dispatch(loadData({count: 40}));
+    }
+
+    filterByInput(e){
+        let input = e.target.value;
+        this.props.dispatch(filterByValue({value: input}))
+    }
+
+    nextPage() {
+        this.props.dispatch(loadNewPage({page: 1}))
+    }
+
+    previousPage() {
+        this.props.dispatch(loadNewPage({page: -1}));
+    }
+
+    goToPage(page) {
+        this.props.dispatch(loadExactPage({page}))
+    }
+
+    sortByInput(e){
+        let value = e.target.value;
+        let direction = value.endsWith('asc') ? "asc" : "desc";
+
+        if (value.startsWith('price')){
+            this.props.dispatch(sortByPrice({direction}))
+        }else {
+            this.props.dispatch(sortByAlphabet({direction}));
+        }
+    }
+
+    render() {
+        let products = this.props.state.filteredProducts;
+        return (
+            <div className="App">
+                <section className="hero">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Pagination, Filtering and Sorting with React
+                            </h1>
+                            <h2 className="subtitle">
+                                A detailed guide
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+                <section className='section'>
+                    <div className='container'>
+                        <nav className="pagination" role="navigation" aria-label="pagination">
+                            <button className="button pagination-previous" onClick={() => {
+                                this.previousPage()
+                            }}>Previous
+                            </button>
+                            <button className="button pagination-next" onClick={() => {
+                                this.nextPage()
+                            }}>Next page
+                            </button>
+                            <ul className="pagination-list">
+                                {
+                                    [...Array(this.props.state.filteredPages)].map((value, index) => (
+                                        <button
+                                            className={`button pagination-link ${this.props.state.currentPage === index + 1 ? "is-current" : ""}`}
+                                            aria-label="Page 1"
+                                            onClick={() => this.goToPage(index + 1)}
+                                            aria-current="page">
+                                            {index + 1}
+                                        </button>
+                                    ))
+                                }
+                            </ul>
+                        </nav>
+
+                    </div>
+                </section>
+                <section className='section'>
+                    <div className='container'>
+                        <div>
+                            <div className="field is-grouped" style={{alignItems: "center"}}>
+                                <div className="control">
+                                    <div className="select">
+                                        <select onChange={e => {
+                                            this.sortByInput(e)
+                                        }}>
+                                            <option value="" disabled selected>Sort by</option>
+
+                                            <option value='alphabet_asc'>Name - A-Z</option>
+                                            <option value='alphabet_desc'>Name - Z-A</option>
+
+                                            <option value='price_asc'>Price - Lowest to Highest</option>
+                                            <option value='price_desc'>Price - Highest to Lowest</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div className='control' style={{minWidth: "300px"}}>
+                                    <input onChange={e => {
+                                        this.filterByInput(e);
+                                    }} style={{width: "100%"}} placeholder='Filter by' type='text'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='tile is-ancestor' style={{flexWrap: "wrap"}}>
+                            {
+                                products && products.length && products.map(product => (
+                                    <div className='tile is-parent is-3'>
+                                        <div className='tile is-child box'>
+                                            <p>
+                                                <b>Name: </b>
+                                                {product.name}
+                                            </p>
+                                            <p>
+                                                <b>Designer: </b>
+                                                {product.designer}
+                                            </p>
+                                            <p>
+                                                <b>Type: </b>
+                                                {product.type}
+                                            </p>
+                                            <p>
+                                                <b>Price: </b>
+                                                ${product.price}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        );
+    }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {state};
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/store/index.js
@@ -0,0 +1,206 @@
+import generate from "../helpers/data";
+const initialState = {
+    appliedFilters: []
+};
+
+const SORT_BY_ALPHABET = "SORT_BY_ALPHABET";
+const SORT_BY_PRICE = "SORT_BY_PRICE";
+const LOAD_DATA = "LOAD_DATA";
+const FILTER_BY_PRICE = "FILTER_BY_PRICE";
+const FILTER_BY_VALUE = "FILTER_BY_VALUE";
+const LOAD_NEW_PAGE = "LOAD_NEW_PAGE";
+const LOAD_EXACT_PAGE = "LOAD_EXACT_PAGE";
+
+export const sortByPrice = payload => ({
+    type: SORT_BY_PRICE,
+    payload
+});
+
+export const filterByPrice = payload => ({
+    type: FILTER_BY_PRICE,
+    payload
+});
+
+export const filterByValue = payload => ({
+    type: FILTER_BY_VALUE,
+    payload
+});
+
+export const sortByAlphabet = payload => ({
+    type: SORT_BY_ALPHABET,
+    payload
+});
+
+export const loadData = (payload) => ({
+    type: LOAD_DATA,
+    payload
+});
+
+export const loadNewPage = (payload) => ({
+    type: LOAD_NEW_PAGE,
+    payload
+});
+
+export const loadExactPage = (payload) => ({
+    type: LOAD_EXACT_PAGE,
+    payload
+});
+
+const filterStore = (state = initialState, action) => {
+    switch (action.type) {
+            case SORT_BY_ALPHABET:
+                const sortByAlphabetState = Object.assign({}, state);
+                let sortedAlphabetArr = action.payload.direction === "asc" ?
+                    sortAsc(state.filteredProducts, 'name') :
+                    sortDesc(state.filteredProducts, 'name');
+
+                sortByAlphabetState.filteredProducts = sortedAlphabetArr;
+                sortByAlphabetState.appliedFilters = addFilterIfNotExists(SORT_BY_ALPHABET, sortByAlphabetState.appliedFilters);
+                sortByAlphabetState.appliedFilters = removeFilter(SORT_BY_ALPHABET, sortByAlphabetState.appliedFilters);
+
+                return sortByAlphabetState;
+        case SORT_BY_PRICE:
+            let sortByPriceState = Object.assign({}, state);
+            let sortedPriceArr = action.payload.direction === "asc" ?
+                sortAsc(state.filteredProducts, 'price') :
+                sortDesc(state.filteredProducts, 'price');
+
+            sortByPriceState.filteredProducts = sortedPriceArr;
+            sortByPriceState.appliedFilters = addFilterIfNotExists(SORT_BY_ALPHABET, sortByPriceState.appliedFilters);
+            sortByPriceState.appliedFilters = removeFilter(SORT_BY_PRICE, sortByPriceState.appliedFilters);
+
+            return sortByPriceState;
+        case FILTER_BY_PRICE:
+            //filter by price
+            return state;
+        case FILTER_BY_VALUE:
+            let newState = Object.assign({}, state);
+            let value = action.payload.value;
+            let filteredValues = state.products.filter(product => {
+                return product.name.toLowerCase().includes(value) ||
+                    product.designer.toLowerCase().includes(value);
+            });
+
+            let appliedFilters = state.appliedFilters;
+
+            if (value) {
+                appliedFilters = addFilterIfNotExists(FILTER_BY_VALUE, appliedFilters);
+
+                newState.filteredProducts = filteredValues;
+                newState.filteredCount = newState.filteredProducts.length;
+                newState.filteredPages = Math.ceil(newState.filteredCount / newState.countPerPage);
+
+            } else {
+                appliedFilters = removeFilter(FILTER_BY_VALUE, appliedFilters);
+
+                if (appliedFilters.length === 0) {
+                    newState.filteredProducts = newState.products;
+                    newState.filteredCount = newState.filteredProducts.length;
+                    newState.filteredPages = Math.ceil(newState.filteredCount / newState.countPerPage);
+                }
+            }
+            return newState;
+        case LOAD_DATA:
+            let count = action.payload.count;
+            let countPerPage = action.payload.countPerPage || 20;
+
+            //round up
+            let totalPages = Math.ceil(count / countPerPage);
+
+            let products = generate(count);
+            return {
+                ...state,
+                products,
+                filteredProducts: products.slice(0, countPerPage),
+                currentCount: countPerPage,
+                countPerPage,
+                totalCount: count,
+                currentPage: 1,
+                totalPages: totalPages,
+                filteredPages: totalPages
+            };
+        case LOAD_NEW_PAGE:
+            //Clone the previous state
+            let loadNewPageState = Object.assign({}, state);
+            //How many pages should be added. Will always be 1 or -1
+            let addPages = action.payload.page;
+            //add it to the current
+            loadNewPageState.currentPage += addPages;
+
+            let perPage = loadNewPageState.countPerPage; //20 by default
+
+            let nextProducts;
+            if (addPages === 1){
+                //Moving from page 1 to 2 will cause ‘upperCount’ to be 40
+                let upperCount = loadNewPageState.currentCount + perPage;
+                let lowerCount = loadNewPageState.currentCount; //This hasn’t been changed. It will remain 20.
+
+                loadNewPageState.currentCount += loadNewPageState.countPerPage;
+                nextProducts = loadNewPageState.products.slice(lowerCount, upperCount);
+            }
+
+            if (addPages ===-1){
+                let upperCount = loadNewPageState.currentCount; //40
+                let lowerCount = loadNewPageState.currentCount - perPage; //20
+
+                loadNewPageState.currentCount -= loadNewPageState.countPerPage;
+                nextProducts = loadNewPageState.products.slice(lowerCount - perPage, upperCount - perPage);
+            }
+
+            loadNewPageState.filteredProducts = nextProducts;
+            window.history.pushState({page: 1}, "title 1", `?page=${loadNewPageState.currentPage}`);
+            return loadNewPageState;
+        case LOAD_EXACT_PAGE:
+            const exactPageState = Object.assign({}, state);
+            const exactPage = action.payload.page;
+            let upperCountExact = exactPageState.countPerPage * exactPage;
+            let lowerCountExact = upperCountExact - exactPageState.countPerPage;
+
+            let exactProducts = exactPageState.products.slice(lowerCountExact, upperCountExact);
+            exactPageState.filteredProducts = exactProducts;
+            exactPageState.currentCount = upperCountExact;
+            exactPageState.currentPage = exactPage;
+            window.history.pushState({page: 1}, "title 1", `?page=${exactPageState.currentPage}`);
+
+            return exactPageState;
+
+        default:
+            return state;
+
+    }
+};
+
+export default filterStore;
+
+function sortAsc(arr, field) {
+    return arr.sort(function (a, b) {
+        if (a[field] > b[field]) return 1;
+
+        if (b[field]> a[field]) return -1;
+
+        return 0;
+    })
+}
+
+function sortDesc(arr, field) {
+    return arr.sort(function (a, b) {
+        if (a[field] > b[field]) return -1;
+
+        if (b[field]> a[field]) return 1;
+
+        return 0;
+    })
+}
+
+function addFilterIfNotExists(filter, appliedFilters) {
+    let index = appliedFilters.indexOf(filter);
+    if (index===-1) appliedFilters.push(filter);
+
+    return appliedFilters;
+}
+
+function removeFilter(filter, appliedFilters) {
+    let index = appliedFilters.indexOf(filter);
+    appliedFilters.splice(index, 1);
+    return appliedFilters;
+}
